cac30c37bfd675e5a47b72c651f0be378544502d
unknown <elhalabiayoub6@gmail.com>
2021-02-07 17:17:42 +0100

    second commit

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -2,18 +2,18 @@ import React from 'react'
 import "./Restaurant.css"
 import StarRatings from 'react-star-ratings';
 
-function Restaurant() {
+function Restaurant({data}) {
     return (
         <div className="restaurant">
             <div className="restaurant__image">
              <img src="http://coveteur.com/wp-content/uploads/2018/03/london-regent-street-best-restaurants-2.jpg"/>
             </div>
             <div className="restaurant__infos">
-                <h2 style={{margin:0}}> nom du restaurant</h2>
+                <h2 style={{margin:0}}> {data.name}</h2>
                 <StarRatings rating={2.403}
                  starDimension="20px"
                       starSpacing="5px" starRatedColor="rgb(255, 165, 0)" starEmptyColor="lightgrey" />
-             <p style={{margin:0}}>adresse du restaurant</p>
+             <p style={{margin:0}}>{data.phone}</p>
                
             </div>
             
--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Restaurant from '../components/Restaurant'
 
 function Restaurants() {
+    const [restaurants , setRestaurants] = useState([])
+    useEffect(()=>{
+      
+        axios.get('http://localhost:8080/restaurants').then((res)=>{
+            setRestaurants(res.data)
+            console.log(restaurants)
+
+        })
+    },[])
     return (
         <div className="restaurants" >
-            <Link to="/restaurant">
-            <Restaurant />
-            </Link>
-            <Restaurant/>
+      {
+          restaurants?.map((restaurant) => (
+              <Link to="/restaurant">
+              <Restaurant data={restaurant} />
+              </Link>
+             
+          ))
+      }
+           
+            
         </div>
     )
 }
